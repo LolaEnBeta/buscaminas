95297a6ee9f916b6b4c10bfe417ad7740b2e748d
LolaEnBeta <lolitaenbeta@gmail.com>
2019-05-08 12:42:15 +0200

    Generate mines

--- buscaminas.py
+++ buscaminas.py
@@ -1,4 +1,4 @@
-
+import random
 board_size = 8
 
 def create_board(board_size):
@@ -16,6 +16,13 @@ def print_board(board):
             print(column, " ", end="")
         print("")
 
+def generate_minas(num_minas, buscaminas):
+    while num_minas > 0:
+        buscaminas[random.randint(0,7)][random.randint(0,7)] = "O"
+        num_minas-=1
+
 buscaminas = create_board(board_size)
+
+generate_minas(10, buscaminas)
 
 print_board(buscaminas)
