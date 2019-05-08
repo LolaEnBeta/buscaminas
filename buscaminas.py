d9d07b5fe55b1a96540496b2443da82a749bc44a
LolaEnBeta <lolitaenbeta@gmail.com>
2019-05-08 16:23:12 +0200

    Calculate neighbours in a specific box

--- buscaminas.py
+++ buscaminas.py
@@ -6,7 +6,7 @@ def create_board(board_size):
     for row in range(board_size):
         row = []
         for column in range(board_size):
-            row.append("*")
+            row.append("-")
         board.append(row)
     return board
 
@@ -18,11 +18,23 @@ def print_board(board):
 
 def generate_minas(num_minas, buscaminas):
     while num_minas > 0:
-        buscaminas[random.randint(0,7)][random.randint(0,7)] = "O"
+        buscaminas[random.randint(1,6)][random.randint(1,6)] = "*"
         num_minas-=1
+
+def calculate_neigbours(current_row, current_column, buscaminas):
+    neighbours = 0
+    for row_index in range(current_row - 1, current_row + 2):
+        for column_index in range(current_column - 1, current_column + 2):
+            if row_index == current_row and column_index == current_column:
+                continue
+            if buscaminas[row_index][column_index] == "*":
+                neighbours += 1
+    buscaminas[current_row][current_column] = neighbours   
 
 buscaminas = create_board(board_size)
 
 generate_minas(10, buscaminas)
+
+calculate_neigbours(1, 1, buscaminas)
 
 print_board(buscaminas)
